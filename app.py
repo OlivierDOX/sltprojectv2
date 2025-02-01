ef1b9bdce5d082634eaf941f57d188196a4dfd5a
OlivierDOX <olivier@doxbrasil.com.br>
2025-02-01 10:05:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -75,13 +75,19 @@ st.dataframe(demand, use_container_width=True)
 
 
 
-def encontra_combinacoes_possiveis(larguras_slitters, largura_bobina):
-    combinacoes = []
-    for n in range(1, largura_bobina // min(larguras_slitters) + 1):
-        for combinacao in combinations_with_replacement(larguras_slitters, n):
-            if sum(combinacao) == largura_bobina:
-                combinacoes.append(combinacao)
-    return combinacoes
+def encontra_combinacoes_possiveis(larguras_slitters, larguras_bobina):
+    todas_combinacoes = []  # Lista para armazenar todas as combinações
+    
+    for largura_bobina in larguras_bobina:
+        combinacoes = []
+        for n in range(1, largura_bobina // min(larguras_slitters) + 1):
+            for combinacao in combinations_with_replacement(larguras_slitters, n):
+                if sum(combinacao) == largura_bobina:
+                    combinacoes.append(combinacao)
+        
+        todas_combinacoes.extend(combinacoes)  # Adiciona todas as combinações à lista
+    
+    return todas_combinacoes
 
 def resolver_problema_corte(larguras_slitters, largura_bobina, peso_bobina, demand):
     proporcao = peso_bobina / largura_bobina
