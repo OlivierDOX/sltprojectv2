9b0e11a1fb7a983eaafcb16aa0ce8e5a5e466c7d
OlivierDOX <olivier@doxbrasil.com.br>
2025-02-12 13:03:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ limite_inferior = st.text_input("Limite Inferior (%)", "90")
 limite_superior = st.text_input("Limite Superior (%)", "130")
 
 # Largura do Slitter
-larguras_bobina = st.number_input("Largura utilizada no Slitter", min_value=1, value=1196, step=1)
+larguras_bobina = [st.number_input("Largura utilizada no Slitter", min_value=1, value=1196, step=1)]
 
 # Peso da bobina 
 peso_bobina = st.number_input("Peso da Bobina", min_value=1, value=23500, step=1)
